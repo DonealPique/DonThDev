1364d8858ad59d9e69f8408040b846b33d1fc5b3
Doneal Piqué <135713732+DonealPique@users.noreply.github.com>
2024-03-12 12:46:11 +0100

    loader added

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -200,3 +200,20 @@ document.addEventListener("DOMContentLoaded", function () {
   let typingContainer = document.querySelector('.typing-container');
   observer.observe(typingContainer);
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  setTimeout(() => {
+      hideLoader();
+      showContent();
+  }, 3000);
+
+  function hideLoader() {
+      const loader = document.getElementById("loader");
+      loader.style.display = "none";
+  }
+
+  function showContent() {
+      const content = document.getElementById("content");
+      content.style.display = "block";
+  }
+});
